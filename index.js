e2e91dd1091696a389ff295c3fdbcfa4d6ce6df0
gabrielly <gabriellycostadossantos063@gmail.com>
2026-07-08 19:12:43 -0300

    aa

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 import express from 'express'
+import bancoDeDados from './repository/index.js'
 
 // server ou app
 const app = express()
 
-app.get("/api/v1/somar", (req, res) => {
-    // query params 
-    const num1 = Number(req.query.num1) 
-    const num2 = Number (req.query.num2) 
-    const resultado = num1+num2
-
-    res.send({ resultado })
-})
-
 // metodo GET da funcao de subtrair usando QuerryParam
 //Com numero 1 e numerou ou num1 e num2
+app.listen(3000,() => {
+    console.log("Servidor ouvindo na porta 3000")
+})
+
+app.get("/api/pessoa/:id", (req, res) => {
+    const id = req.params.id
+
+   const pessoa = bancoDeDados.find(it => it.id == id)
+    if (!pessoa){
+       res.send({ message: "Pessoa nao encontrada" })
+   return  
+    }
+  res.send({ pessoa })
+})
+
+app.get("/api/pessoa", (req, res) => {
+  const { id, name } = req.query
+
+  if(!id|| !name) {
+    res.send({ message: "Favor informar id e name" })
+    return
+  }
+ bancoDeDados.push({ id, name})
+ res.send({ message: "Pessoa criada com sucesso"})
+})
 app.listen(3000,() => {
     console.log("Servidor ouvindo na porta 3000")
 })
