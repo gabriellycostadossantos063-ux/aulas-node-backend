a8db43fcd6afb4d2d2535d1251272589e65d4894
gabrielly <gabriellycostadossantos063@gmail.com>
2026-08-03 19:20:11 -0300

    a

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
 import express from 'express'
-import bancoDeDados from './repository/index.js'
+import router from './router/exercicios.js'
 
 // server ou app
 const app = express()
 
-// metodo GET da funcao de subtrair usando QuerryParam
-//Com numero 1 e numerou ou num1 e num2
+app.use(express.json())
+app.use("/api/v1",router)
+
+
+
+
+
+
 app.listen(3000,() => {
     console.log("Servidor ouvindo na porta 3000")
-})
-
-app.get("/api/pessoa/:id", (req, res) => {
-    const id = req.params.id
-
-   const pessoa = bancoDeDados.find(it => it.id == id)
-    if (!pessoa){
-       res.send({ message: "Pessoa nao encontrada" })
-   return  
-    }
-  res.send({ pessoa })
-})
-
-app.get("/api/pessoa", (req, res) => {
-  const { id, name } = req.query
-
-  if(!id|| !name) {
-    res.send({ message: "Favor informar id e name" })
-    return
-  }
- bancoDeDados.push({ id, name})
- res.send({ message: "Pessoa criada com sucesso"})
-})
-app.listen(3000,() => {
-    console.log("Servidor ouvindo na porta 3000")
-})
-
-app.post("/api/valor",(req, res) => {
-    const valor 
 })
