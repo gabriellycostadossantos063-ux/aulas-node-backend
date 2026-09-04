c4ab3dc2e53060f86fcc06ae6dea5dbb817d670f
gabrielly <gabriellycostadossantos063@gmail.com>
2026-09-04 19:18:46 -0300

    aa

--- controller/carro.js
+++ controller/carro.js
@@ -1,68 +1,75 @@
 import ServiceCarro from '../service/carro.js'
-
+ 
 class ControllerCarro {
-
-    //Recebimento e a Sai´da das info
-    Buscar(_, res)  {
+    // Recebimento e a Saida das info
+    async Buscar(_, res) {  
         try {
-            const carros = ServiceCarro.Buscar()
-            res.send({ mensagem: carros })
-        } catch (error) {
-            res.status(500).send({
-                mensage: error.message
-            })
-        }
-    }
-    Detalhe(req, res)  {
-    try {
-        const id = req.params.id
-        const carro = ServiceCarro.Detalhe(id)
-        
-
-    res.send({ mensagem: carro})
-
-    }catch (error) {
-            res.status(500).send({
-                mensage: error.message
-            })
-        }
-    
-    }
-
-    Criar(req, res)  {
-    const { id,marca,ano } = req.body
-
-    ServiceCarro.Criar(id, marca, ano)
-    
-    res.send({ mensagem: "Cadastrado com sucesso"})
-}
-
-    Alterar(req, res) {
-        try {
-        res.send({ mensagem: carros })
-        } catch (error) {
-            res.status(500).send({
-                mensage: error.message
-            })
-        }
-        
-    }
-    
-    Deletar(req, res) {
-        try{
-            const identificador = req.body.id
-
-            ServiceCarro.Deletar(identificador)
-            
-            res.send({ mensagem: "Deletado" })
+            const carros = await ServiceCarro.Buscar()
+            res.status(200).send({ mensagem: carros })
         } catch (error) {
             res.status(500).send({
                 mensagem: error.message
             })
         }
     }
+ 
+    async Detalhe(req, res) {
+        try {
+            const id = req.params.id
+ 
+            const carro = await ServiceCarro.Detalhe(id)
+ 
+            res.status(200).send({ mensagem: carro })
+        } catch (error) {
+            res.status(500).send({
+                mensagem: error.message
+            })
+        }
+    }
+ 
+    async Criar(req, res) {
+        try {
+            const { marca, ano } = req.body
+ 
+            await ServiceCarro.Criar(marca, ano)
+           
+            res.status(201).send({ mensagem: "Cadastrado com sucesso" })
+        } catch (error) {
+            res.status(500).send({
+                mensagem: error.message
+            })
+        }
+    }
+ 
+    async Alterar(req, res) {
+        try {
+            const { marca, ano } = req.body
+            const id = req.params.id
+ 
+            await ServiceCarro.Alterar(id, marca, ano)
+           
+            res.status(201).send({ mensagem: "Cadastrado com sucesso" })
+        } catch (error) {
+            res.status(500).send({
+                mensagem: error.message
+            })
+        }
+    }
+ 
+    async Deletar(req, res) {
+        try {
+            const identificador = req.params.id
+ 
+            await ServiceCarro.Deletar(identificador)
+ 
+            res.status(204).send({ mensagem: "Deletado" })
+        } catch (error) {
+           
+            res.status(500).send({
+                mensagem: error.message
+            })
+        }
+    }
 }
-
-    
-
-export default  new ControllerCarro()
+ 
+export default new ControllerCarro()
--- service/carro.js
+++ service/carro.js
@@ -1,42 +1,56 @@
 import RepositoryCarro from '../repository/carro.js'
-
+ 
 class ServiceCarro {
-    Buscar() {
-        return RepositoryCarro
+ 
+    // Core- Regra de Negocio
+    async Buscar() {
+        return RepositoryCarro.find()
     }
-
-    Detalhe(id) {
-        if (!id) {
-            throw Error("Favor informar o ID")
+ 
+    async Detalhe(id) {
+        if(!id) {
+            throw new Error("Favor informar o ID")
         }
-        const carro = RepositoryCarro.find(it => it.id === id)
-
-        if (!carro) {
+ 
+        const carro = await RepositoryCarro.findById(id)
+       
+        if(!carro) {
             throw new Error(`ID ${id} do carro não encontrado`)
         }
+ 
         return carro
     }
-    //Função(parametros)
-    Criar(id, marca, ano) {
-        if (!id || !marca || !ano) {
-            throw new Error("Favor informar todos os dados")
-        }
-        RepositoryCarro.push({ id, marca, ano })
-
-        return { id, marca, ano }
+    // Função(parametros, parametros, parametros)
+    async Criar(marca, ano) {
+         if (!marca || !ano) {
+             throw new Error("Favor informar todos os dados")
+         }
+         const carro = await RepositoryCarro.Create( marca, ano )
+ 
+         return carro
     }
-
-    Alterar() { }
-
-    Deletar(id) {
+ 
+    async Alterar(id, marca, ano) {
+        if (!id || !marca || !ano) {
+            throw new Error("Favor informar os dados");
+        }
+ 
+        const carroAlterado = await RepositoryCarro.Update(id, marca, ano)
+ 
+        return carroAlterado
+    }
+ 
+    async Deletar(id) {
+ 
         if (!id) {
             throw new Error("Favor informar o ID")
         }
-
-        RepositoryCarro.splice(it => it.id === id, 1)
-        return id
+       
+        const carro = await RepositoryCarro.Delete(id)
+ 
+        return carro
     }
-
+ 
 }
-
+ 
 export default new ServiceCarro()
